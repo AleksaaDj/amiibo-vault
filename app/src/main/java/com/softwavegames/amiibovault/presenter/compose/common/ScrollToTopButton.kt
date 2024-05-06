83b151b074aa076e72d360d47670a8d34fe37655
Aleksa Djordjevic <aleksa.ddjordjevic@gmail.com>
2024-05-06 23:29:33 +0200

    Added scroll to top and nfc image.

---
+++ app/src/main/java/com/softwavegames/amiibovault/presenter/compose/common/ScrollToTopButton.kt
@@ -0,0 +1,39 @@
+package com.softwavegames.amiibovault.presenter.compose.common
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ScrollToTopButton(onClick: () -> Unit) {
+    Box(
+        Modifier
+            .fillMaxSize()
+            .padding(bottom = 20.dp, end = 20.dp), Alignment.BottomEnd
+    ) {
+        FloatingActionButton(
+            onClick = { onClick() },
+            modifier = Modifier
+                .size(50.dp),
+            containerColor = MaterialTheme.colorScheme.surface,
+        ) {
+            Icon(
+                Icons.Filled.KeyboardArrowUp,
+                contentDescription = null,
+                tint = Color.White,
+                modifier = Modifier.padding(10.dp)
+            )
+        }
+    }
+}
